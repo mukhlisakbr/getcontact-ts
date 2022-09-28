5e0308fd2755d93fd9b95c1ab0b70b9d6ba545a3
Mukhlis Akbarrudin <mukhlis3389@gmail.com>
2022-09-28 22:23:35 +0700

    chore: update test

--- src/index.test.ts
+++ src/index.test.ts
@@ -5,6 +5,6 @@ it("Getcontact checkNumber should get response", async () => {
   const getcontact = new Getcontact(process.env.TOKEN!, process.env.KEY!);
   // check kemenkes number
   const res = await getcontact.checkNumber("081110500567");
-  console.log(JSON.stringify(res));
+  console.dir(res, { depth: null });
   expect(res).toBeDefined();
 });
