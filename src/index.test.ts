549cee6bc5a274629fdc6b89db06e23d1d29d3d1
Mukhlis Akbarrudin <27577560+mukhlisakbr@users.noreply.github.com>
2023-07-19 10:45:57 +0700

    Log tags in test

--- src/index.test.ts
+++ src/index.test.ts
@@ -5,6 +5,6 @@ it("Getcontact checkNumber should get response", async () => {
   const getcontact = new Getcontact(process.env.TOKEN!, process.env.KEY!);
   // check kemenkes number
   const res = await getcontact.checkNumber("081110500567");
-  console.dir(res, { depth: null });
+  console.dir(res.result.tags, { depth: null });
   expect(res).toBeDefined();
 });
