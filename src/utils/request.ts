20432d72d9f0179db5ef864e298ac26458f7a0c2
Mukhlis Akbarrudin <27577560+mukhlisakbr@users.noreply.github.com>
2023-07-19 10:35:52 +0700

    Handling axios post error

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -2,5 +2,15 @@ import crypto from "./crypto";
 import axios from "axios";
 
 export default async (hexUrl: any, data: any, headers: any) => {
-  return axios.post(crypto.hexToUtf8(hexUrl), { data }, { headers });
+  return axios
+    .post(crypto.hexToUtf8(hexUrl), { data }, { headers })
+    .then((response) => {
+      console.log("Response Data:", response.data);
+      return response;
+    })
+    .catch((error) => {
+      const errorMessage =
+        error.message || "An error occurred during the request.";
+      throw new Error(errorMessage);
+    });
 };
